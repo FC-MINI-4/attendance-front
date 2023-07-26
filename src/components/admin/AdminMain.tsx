412262d567bf3c29b1aca3c9b2b15b1b816793ee
leechanghwi <lih120@naver.com>
2023-07-26 22:45:45 +0900

    FEAT:사이드바UI작업

--- src/components/admin/AdminMain.tsx
+++ src/components/admin/AdminMain.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MainIProps } from '@/types/IAdmin';
+import MainHeader from './MainHeader';
+import SideBar from './SideBar';
 
 export default function Main({ page }: MainIProps) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const handleToggleSidebar = () => {
+    setIsSidebarOpen(prevIsSidebarOpen => !prevIsSidebarOpen);
+  };
+
   return (
     <div className="w-[65rem] h-[50rem] bg-white mx-auto rounded-2xl ">
-      <div className="h-[4rem] border-solid border-b-2 flex items-center justify-between">
-        <div className="flex">
-          <button className="ml-4 mr-[1rem]">토글버튼</button>
-          <div>회사로고</div>
-        </div>
-        <div className="flex">
-          <div className="mr-[1rem]">관리자님, 환영합니다!</div>
-          <button className="mr-4">로그아웃</button>
-        </div>
-      </div>
+      <MainHeader onToggleSidebar={handleToggleSidebar} />
       <div className=" flex">
-        <div className="w-[5rem] h-[46rem]  bg-primary rounded-bl-2xl ">
-          사이드바자리 사이드바컴포넌트로 뺼예정
-        </div>
+        {isSidebarOpen && <SideBar />}
         <div className="">
           <div className="w-[10rem] h-[2rem]  font-400 mb-[3rem] text-3xl flex ml-[2rem] mt-[2rem]">
             {page === 'admin-manage' && '관리'}
@@ -25,16 +22,34 @@ export default function Main({ page }: MainIProps) {
             {page === 'admin-duty' && '당직요청관리'}
           </div>
           <div
-            className=" ml-[2rem] flex border-solid border-b-2 
-          ">
-            <div className="mr-[4rem]">요청</div>
-            <div className="mr-[4rem]">이름</div>
-            <div className="mr-[4rem]">부서명</div>
-            <div className="mr-[5rem]">근속기간</div>
-            <div className="mr-[6rem]">요청 날짜</div>
-            <div className="mr-[6rem]">요청 사유</div>
-            <div className="mr-[5rem]">상태</div>
-            <div className="">관리</div>
+            className={`flex border-solid border-b-2 justify-between ${
+              isSidebarOpen ? 'w-[60rem]' : 'w-[65rem]'
+            }`}>
+            {(page === 'admin-duty' || page === 'admin-leave') && (
+              <>
+                <div className="ml-[2rem]">요청</div>
+                <div className="">이름</div>
+                <div className="">부서명</div>
+                <div className="">직급</div>
+                <div className="">입사일</div>
+                <div className="">요청사항</div>
+                <div className="">상태</div>
+                <div className="mr-[3rem]">관리</div>
+              </>
+            )}
+
+            {page === 'admin-manage' && (
+              <>
+                <div className="ml-[2rem]">이름</div>
+                <div className="">부서명</div>
+                <div className="">직급</div>
+                <div className="">입사일</div>
+                <div className="">당직내역</div>
+                <div className="">총연차</div>
+                <div className="">사용연차</div>
+                <div className="mr-[4rem]">잔여연차</div>
+              </>
+            )}
           </div>
         </div>
       </div>
