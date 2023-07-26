ad91cba122b5367563122dd08c3730fd19459cf9
Kled <1017yu@daum.net>
2023-07-26 13:17:50 +0900

    settings: dev 브랜치 세팅

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
+import Button from '@/components/common/Button';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
       <Link href="/signin">Go to Sign-In</Link>
+      <Button contents={'확인'} secondary></Button>
+      <br />
+      <Button contents={'확인'} disabled></Button>
+      <br />
+      <Button contents={'확인'} submit></Button>
+      <br />
+      <Button contents={'확인'}></Button>
     </div>
   );
 }
