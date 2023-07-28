bf64ebb761d380584e543af8dffe627c4e415394
Kled <1017yu@daum.net>
2023-07-28 15:56:47 +0900

    FEAT: 비밀번호 찾기 UI 작업

---
+++ src/components/auth/AuthFindPwBox.tsx
@@ -0,0 +1,21 @@
+import PwBox from '@/components/common/PwBox';
+import Button from '@/components/common/Button';
+import AuthFindPwInput from '@/components/auth/AuthFindPwInput';
+
+export default function AuthFindPwBox() {
+  return (
+    <PwBox>
+      <div className="sm:mb-8 sm:text-3xl">
+        계정의
+        <br /> 비밀번호를 재설정 합니다.
+      </div>
+      <div className="text-subTextAndBorder  sm:mb-8">
+        비밀번호를 재설정할 계정의 <br /> 이메일을 입력해주세요. <br />
+        <br />
+        가입하신 이메일로 임시 비밀번호를 발송해드립니다.
+      </div>
+      <AuthFindPwInput />
+      <Button contents={'이메일 전송'} />
+    </PwBox>
+  );
+}
---
+++ src/components/auth/AuthFindPwInput.tsx
@@ -0,0 +1,15 @@
+import Input from '@/components/common/Input';
+
+export default function AuthFindPwInput() {
+  const onClick = () => {};
+  return (
+    <div className="sm:mb-16">
+      <Input
+        label={'이메일'}
+        name={'email'}
+        onChange={onClick}
+        placeholder={'예: dangyeon@dangyeon.com'}
+      />
+    </div>
+  );
+}
