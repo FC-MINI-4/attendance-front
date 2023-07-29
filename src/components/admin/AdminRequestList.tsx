dca41cf96be4b3bbe39abd789cb552bebf53489c
leechanghwi <lih120@naver.com>
2023-07-29 20:53:44 +0900

    FEAT:필터기능추가

--- src/components/admin/AdminRequestList.tsx
+++ src/components/admin/AdminRequestList.tsx
@@ -1,12 +1,98 @@
-import React, { useState } from 'react';
-import { SideBarIProps } from '@/types/IAdmin';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import {
+  SideBarIProps,
+  FilterIProps,
+  MainIProps,
+  LeaveResIProps
+} from '@/types/IAdmin';
 
-export const RequestList: React.FC<SideBarIProps> = ({ isSidebarOpen }) => {
-  const [status, setStatus] = useState('대기중');
+export const RequestList: React.FC<
+  SideBarIProps & FilterIProps & MainIProps
+> = ({
+  isSidebarOpen,
+  searchValue,
+  selectedDepartment,
+  selectedPosition,
+  selectedDuty,
+  selectedRest,
+  selectedStatus,
+  page
+}) => {
+  const [employees, setEmployees] = useState<LeaveResIProps[]>([]);
+  const [status, setStatus] = useState('');
+  const [filteredEmployees, setFilteredEmployees] = useState<LeaveResIProps[]>(
+    []
+  );
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      try {
+        const response = await axios.get('/api/RestRequest');
+        const responseData = response.data;
+        console.log('API 응답 데이터:', responseData);
+        setEmployees(responseData.data?.employees || []);
+      } catch (error) {
+        console.error('API 호출 오류:', error);
+      }
+    };
+    fetchEmployees();
+  }, []);
+
+  useEffect(() => {
+    const filterEmployees = () => {
+      let newFilteredEmployees = employees;
+
+      if (selectedDepartment !== '부서명') {
+        newFilteredEmployees = newFilteredEmployees.filter(
+          employee => employee.department === selectedDepartment
+        );
+      }
+
+      if (selectedPosition !== '직급') {
+        newFilteredEmployees = newFilteredEmployees.filter(
+          employee => employee.position === selectedPosition
+        );
+      }
+      if (page === 'admin-leave' && selectedRest !== '요청') {
+        newFilteredEmployees = newFilteredEmployees.filter(
+          employee => employee.type === selectedRest
+        );
+      }
+
+      if (page === 'admin-duty' && selectedDuty !== '요청') {
+        newFilteredEmployees = newFilteredEmployees.filter(
+          employee => employee.type === selectedDuty
+        );
+      }
+
+      if (selectedStatus !== '상태') {
+        newFilteredEmployees = newFilteredEmployees.filter(
+          employee => employee.status === selectedStatus
+        );
+      }
+
+      if (searchValue !== '') {
+        newFilteredEmployees = newFilteredEmployees.filter(employee =>
+          employee.name.includes(searchValue.trim())
+        );
+      }
+
+      setFilteredEmployees(newFilteredEmployees);
+    };
+    filterEmployees();
+  }, [
+    selectedDepartment,
+    selectedPosition,
+    searchValue,
+    selectedStatus,
+    selectedDuty,
+    selectedRest,
+    employees,
+    page
+  ]);
 
   const handleApproval = () => {
     if (window.confirm('승인 후 수정불가능합니다. 승인하시겠습니까?')) {
-      setStatus('승인됨');
       alert('수정되었습니다.');
     } else {
       alert('취소되었습니다.');
@@ -15,7 +101,6 @@ export const RequestList: React.FC<SideBarIProps> = ({ isSidebarOpen }) => {
 
   const handleRejection = () => {
     if (window.confirm('거절 후 수정불가능합니다. 거절하시겠습니까?')) {
-      setStatus('거절됨');
       alert('거절되었습니다.');
     } else {
       alert('취소되었습니다.');
@@ -23,45 +108,69 @@ export const RequestList: React.FC<SideBarIProps> = ({ isSidebarOpen }) => {
   };
 
   return (
-    <div
-      className={`flex border-solid border-b-[1px] justify-between h-[42px] items-center `}>
-      <div className="w-[8rem] text-center font-semibold">연차</div>
-      <div className="w-[12rem] text-center">이창휘</div>
-      <div className="w-[8.5rem] ml-4 text-center">인사팀</div>
-      <div className="w-[8.5rem] text-center">사원</div>
-      <div className="text-center w-[13rem]">2021년 11월 27일</div>
-      <button className="w-[18rem] justify-center flex hover:underline  text-secondaryGray text-center">
-        2023년 12월 24일 ~ 2023년 12월 25일
-      </button>
-
-      <div className="w-[10rem] item-center flex justify-center">
+    <>
+      {filteredEmployees.map(employee => (
         <div
-          className={`w-[4rem] h-[28px] text-white rounded-md item-center flex justify-center ${
-            status === '대기중'
-              ? 'bg-mainOrange'
-              : status === '거절됨'
-              ? 'bg-secondary'
-              : 'bg-primary'
-          }`}>
-          {status}
+          key={employee.employeeId}
+          className={`flex border-solid border-b-[1px] justify-between h-[42px] items-center `}>
+          {page === 'admin-duty' && (
+            <div className="w-[8rem] text-center font-semibold">당직</div>
+          )}
+          {page === 'admin-leave' && (
+            <div className="w-[8rem] text-center font-semibold">
+              {employee.type}
+            </div>
+          )}
+          <div className="w-[12rem] text-center">{employee.name}</div>
+          <div className="w-[8.5rem]  text-center">{employee.department}</div>
+          <div className="w-[7.5rem] text-center"> {employee.position}</div>
+          <div className="text-center w-[13rem]">{employee.hireDate}</div>
+          <button className="w-[18rem] justify-center flex hover:underline  text-secondaryGray text-center">
+            {`${employee.startDate} ~ ${employee.endDate}`}
+          </button>
+
+          <div className="w-[10rem] item-center flex justify-center">
+            {(() => {
+              if (employee.status === '대기중') {
+                return (
+                  <div className="w-[4rem] h-[28px] text-white rounded-md item-center flex justify-center bg-mainOrange">
+                    {employee.status}
+                  </div>
+                );
+              } else if (employee.status === '거절됨') {
+                return (
+                  <div className="w-[4rem] h-[28px] text-white rounded-md item-center flex justify-center bg-secondary">
+                    {employee.status}
+                  </div>
+                );
+              } else {
+                return (
+                  <div className="w-[4rem] h-[28px] text-white rounded-md item-center flex justify-center bg-primary">
+                    {employee.status}
+                  </div>
+                );
+              }
+            })()}
+          </div>
+
+          <div className="w-[12rem] text-center">
+            {employee.status === '대기중' && (
+              <>
+                <button
+                  onClick={handleApproval}
+                  className="w-[4rem] border-solid border-2 rounded-md mr-1 border-primary text-primary">
+                  승인
+                </button>
+                <button
+                  onClick={handleRejection}
+                  className="w-[4rem] border-solid border-2 rounded-md border-secondary text-secondary">
+                  거절
+                </button>
+              </>
+            )}
+          </div>
         </div>
-      </div>
-      <div className="w-[12rem] text-center">
-        {status === '대기중' && (
-          <>
-            <button
-              onClick={handleApproval}
-              className="w-[4rem] border-solid border-2 rounded-md mr-1 border-primary text-primary">
-              승인
-            </button>
-            <button
-              onClick={handleRejection}
-              className="w-[4rem] border-solid border-2 rounded-md border-secondary text-secondary">
-              거절
-            </button>
-          </>
-        )}
-      </div>
-    </div>
+      ))}
+    </>
   );
 };
--- src/pages/api/RestRequest.ts
+++ src/pages/api/RestRequest.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { ManageIProps } from '@/types/IAdmin';
+import { LeaveIProps } from '@/types/IAdmin';
 
 const dummyEmployees = [
   {
@@ -7,10 +7,14 @@ const dummyEmployees = [
     name: '이창휘',
     department: '개발팀',
     position: '사원',
-    hireDate: '2022-9-4',
-    dayOffTotal: 15,
-    dayOffUsed: 7,
-    dayOffRemains: 8
+    hireDate: '2021-11-27',
+    dayOffId: 1,
+    requestDate: '2023-07-01',
+    type: '연차',
+    status: '거절됨',
+    startDate: '2023-07-1',
+    endDate: '2023-07-02',
+    reason: '쉬고 싶어요.'
   },
   {
     employeeId: 2,
@@ -18,9 +22,13 @@ const dummyEmployees = [
     department: '기획팀',
     position: '부장',
     hireDate: '2022-1-27',
-    dayOffTotal: 15,
-    dayOffUsed: 7,
-    dayOffRemains: 8
+    dayOffId: 2,
+    requestDate: '2023-07-09',
+    type: '연차',
+    status: '승인됨',
+    startDate: '2023-06-21',
+    endDate: '2023-07-02',
+    reason: '놀고 싶어요.'
   },
 
   {
@@ -29,9 +37,13 @@ const dummyEmployees = [
     department: '영업팀',
     position: '대리',
     hireDate: '2020-7-5',
-    dayOffTotal: 15,
-    dayOffUsed: 7,
-    dayOffRemains: 8
+    dayOffId: 3,
+    requestDate: '2023-04-09',
+    type: '연차',
+    status: '대기중',
+    startDate: '2023-06-30',
+    endDate: '2023-07-02',
+    reason: '놀고 싶어요.'
   },
 
   {
@@ -40,9 +52,13 @@ const dummyEmployees = [
     department: '인사팀',
     position: '부장',
     hireDate: '2022-7-27',
-    dayOffTotal: 15,
-    dayOffUsed: 6,
-    dayOffRemains: 9
+    dayOffId: 4,
+    requestDate: '2023-03-09',
+    type: '반차',
+    status: '대기중',
+    startDate: '2023-03-20',
+    endDate: '2023-03-022',
+    reason: '집가고 싶어요.'
   },
 
   {
@@ -51,9 +67,13 @@ const dummyEmployees = [
     department: '회계팀',
     position: '차장',
     hireDate: '2017-5-25',
-    dayOffTotal: 15,
-    dayOffUsed: 4,
-    dayOffRemains: 11
+    dayOffId: 5,
+    requestDate: '2023-03-09',
+    type: '연차취소',
+    status: '대기중',
+    startDate: '2023-03-20',
+    endDate: '2023-03-22',
+    reason: '집가고 싶어요.'
   },
 
   {
@@ -62,9 +82,13 @@ const dummyEmployees = [
     department: '법무팀',
     position: '차장',
     hireDate: '2004-4-23',
-    dayOffTotal: 15,
-    dayOffUsed: 4,
-    dayOffRemains: 11
+    dayOffId: 6,
+    requestDate: '2023-02-09',
+    type: '반차취소',
+    status: '대기중',
+    startDate: '2023-04-20',
+    endDate: '2023-04-22',
+    reason: '집가고 싶어요.'
   },
   {
     employeeId: 7,
@@ -72,19 +96,23 @@ const dummyEmployees = [
     department: '영업팀',
     position: '과장',
     hireDate: '2007-3-7',
-    dayOffTotal: 15,
-    dayOffUsed: 4,
-    dayOffRemains: 11
+    dayOffId: 7,
+    requestDate: '2022-1-09',
+    type: '연차취소',
+    status: '대기중',
+    startDate: '2022-03-20',
+    endDate: '2022-03-22',
+    reason: '집가고 싶어요.'
   }
 ];
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ManageIProps>
+  res: NextApiResponse<LeaveIProps>
 ) {
   if (req.method === 'GET') {
     try {
-      const resData: ManageIProps = {
+      const resData: LeaveIProps = {
         success: true,
         code: 'SUCCESS',
         message: '성공했습니다.',
