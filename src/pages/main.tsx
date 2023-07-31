cf84302fcfc7351ef11d56d1a633bccb587db123
96uoow <mhs0704123@naver.com>
2023-07-31 10:35:45 +0900

    승인모달기본틀

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,19 +1,28 @@
 import Calendar from '@/components/main/MainCalendar';
 import MainHeader from '@/components/main/MainHeader';
 import SideMenu from '@/components/main/MainSideMenu';
+import ApproveModal from '@/components/common/Modal';
 
 export default function main(){
   return(
-  <div className='w-screen h-screen position'>
-    <div className='absolute top-0 left-0 w-full shadow-md'>
-      <MainHeader/>
+  <>
+    <ApproveModal
+      title={'연차/반차 등록'}
+      IsCheckBoxShow={true}
+      IsTextBoxShow={true}
+      submit={'승인 요청'}/>
+    <div className='w-screen h-screen position'>
+      <div className='absolute top-0 left-0 w-full shadow-md'>
+        <MainHeader/>
+      </div>
+      <div className='absolute top-[8.5rem] left-0 w-1/6 pt-10 pl-3 pr-3'>
+        <SideMenu/>
+      </div>
+      <div className='absolute top-[8.5rem] right-0 w-5/6'>
+        <Calendar/>
+      </div>
     </div>
-    <div className='absolute top-[8.5rem] left-0 w-1/6 pt-10 pl-3 pr-3'>
-      <SideMenu/>
-    </div>
-    <div className='absolute top-[8.5rem] right-0 w-5/6'>
-      <Calendar/>
-    </div>
-  </div>
+  </>
+
   )
 }
