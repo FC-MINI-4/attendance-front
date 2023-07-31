25fda2e4cab0d171e51b68dbee273b3fc91f8781
Kled <1017yu@daum.net>
2023-07-31 19:36:54 +0900

    FEAT: 비밀번호 확인 체크 기능 추가

--- src/components/auth/AuthCheckPw.tsx
+++ src/components/auth/AuthCheckPw.tsx
@@ -2,5 +2,9 @@ import { IAuthCheckPw } from '@/types/IAuth';
 
 // 비밀번호 확인 체크 컴포넌트
 export default function AuthCheckPw({ pwd, checkedPwd }: IAuthCheckPw) {
-  return <div>{pwd === checkedPwd ? 'O' : 'X'}</div>;
+  return (
+    <div>
+      {pwd === checkedPwd && pwd.length > 8 ? '비밀번호가 일치합니다.' : 'X'}
+    </div>
+  );
 }
