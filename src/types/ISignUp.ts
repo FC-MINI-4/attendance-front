6a039fd7327a2a225c4b6bcf06d9f1802cbd112b
Kled <1017yu@daum.net>
2023-07-31 19:41:45 +0900

    FEAT: 이메일 유효성 검사 기능 추가

---
+++ src/types/ISignUp.ts
@@ -0,0 +1,14 @@
+export interface ISignUp {
+  profileUrl: string;
+  position: string;
+  name: string;
+  email: string;
+  password: string;
+  checkedPwd: string;
+  phone: string;
+  hireDate: string;
+}
+
+export interface IRegexCheck {
+  valid?: boolean;
+}
