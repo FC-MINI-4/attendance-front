d63bfd55b7c8c02555a3693bcfb4ef4739a10983
Kled <1017yu@daum.net>
2023-07-31 19:42:53 +0900

    FIX: 회원가입 관련 Props, 타입 변경

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,3 +1,4 @@
+import { RecoilRoot } from 'recoil';
 import Button from '@/components/common/Button';
 import { SIGNUP_INPUT_INFO } from '@/constants/constants';
 import AuthSignUpInput from '@/components/auth/sign-up/AuthSignUpInput';
@@ -5,19 +6,21 @@ import AuthSignUpTitle from '@/components/auth/sign-up/AuthSignUpTitle';
 
 export default function SignUp() {
   return (
-    <div className="mx-auto sm:w-[500px] sm:my-[90px]">
-      <AuthSignUpTitle />
-      {SIGNUP_INPUT_INFO.map(value => (
-        <AuthSignUpInput
-          key={value.label}
-          info={value.label}
-          button={value.button}
-          placeholder={value.placeholder}
-          enLabel={value.enLabel}
-          type={value.type}
-        />
-      ))}
-      <Button contents={'회원가입'} />
-    </div>
+    <RecoilRoot>
+      <div className="mx-auto sm:w-[500px] sm:my-[90px]">
+        <AuthSignUpTitle />
+        {SIGNUP_INPUT_INFO.map(value => (
+          <AuthSignUpInput
+            key={value.label}
+            label={value.label}
+            button={value.button}
+            placeholder={value.placeholder}
+            name={value.name}
+            type={value.type}
+          />
+        ))}
+        <Button contents={'회원가입'} />
+      </div>
+    </RecoilRoot>
   );
 }
