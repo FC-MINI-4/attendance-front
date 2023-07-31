eb595a6793263d64b246ef4b6a49f149394468fe
Kled <1017yu@daum.net>
2023-07-31 19:41:45 +0900

    FEAT: 이메일 유효성 검사 기능 추가

--- src/components/auth/sign-up/AuthSignUpInput.tsx
+++ src/components/auth/sign-up/AuthSignUpInput.tsx
@@ -1,39 +1,24 @@
-import { useState } from 'react';
+import { useRecoilState } from 'recoil';
+import AuthPwCheck from './AuthPwCheck';
+import AuthEmailCheck from './AuthEmailCheck';
+import { signUpState } from '@/recoil/signUp';
 import Input from '@/components/common/Input';
-import { IAuthSignUpInput } from '@/types/IAuth';
 import Button from '@/components/common/Button';
+import { IAuthSignUpInput } from '@/types/IAuth';
 import AuthCheckPw from '@/components/auth/AuthCheckPw';
+import { rEmail, rPassword } from '@/constants/constants';
 
-export default function AuthSignUpInput({
-  info,
-  button,
-  placeholder,
-  enLabel,
-  type
-}: IAuthSignUpInput) {
-  const [signupInput, setSignupInput] = useState({
-    profileUrl: '',
-    position: '',
-    name: '',
-    email: '',
-    password: '',
-    checkedPwd: '',
-    phone: '',
-    hireDate: ''
-  });
+export default function AuthSignUpInput({ ...props }: IAuthSignUpInput) {
+  // 회원가입 정보 atom state 구독
+  const [signUpInfo, setSignUpInfo] = useRecoilState(signUpState);
 
-  const { password, checkedPwd } = signupInput;
-
-  // console.log(`password: ${password}`);
-  // console.log(`checked : ${checkedPwd}`);
-
-  // input 입력 값 처리
+  // input 입력 값 atom state 업데이트
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
-    setSignupInput({
-      ...signupInput,
+    setSignUpInfo(prevInformation => ({
+      ...prevInformation,
       [name]: value
-    });
+    }));
   };
 
   // 이메일 중복 체크 (업데이트 전)
@@ -41,35 +26,90 @@ export default function AuthSignUpInput({
     alert('사용 가능한 이메일입니다.');
   };
 
-  // 조건부 버튼 렌더링
+  // 버튼 & 비밀번호 확인 조건부 렌더링
   const renderButton = () => {
-    if (button) {
-      if (button === '중복확인')
+    // button props가 있을 때
+    if (props.button) {
+      // 중복확인 버튼일 경우
+      if (props.button === '중복확인')
         return (
-          <Button contents={button} onClick={onDuplicateClick} secondary />
+          <Button
+            contents={props.button}
+            onClick={onDuplicateClick}
+            secondary
+          />
         );
+      // 업로드 버튼일 경우
       else {
         return (
-          <Button contents={button} onClick={onDuplicateClick} secondary />
+          <Button
+            contents={props.button}
+            onClick={onDuplicateClick}
+            secondary
+          />
         );
       }
-    } else if (info === '비밀번호 확인') {
-      return <AuthCheckPw pwd={password} checkedPwd={checkedPwd} />;
+      // 비밀번호 확인일 경우
+    } else if (props.label === '비밀번호 확인') {
+      return (
+        <AuthCheckPw
+          pwd={signUpInfo.password}
+          checkedPwd={signUpInfo.checkedPwd}
+        />
+      );
     } else return;
   };
 
+  // 이메일 형식 유효성 체크
+  const emailCheck = () => {
+    if (signUpInfo.email.trim() === '') {
+      return true;
+    }
+
+    return rEmail.test(signUpInfo.email);
+  };
+
+  // 비밀번호 형식 유효성 체크
+  const passwordCheck = () => {
+    if (signUpInfo.password.trim() === '') {
+      return true;
+    }
+
+    return rEmail.test(signUpInfo.password);
+  };
+
+  const renderValid = () => {
+    if (props.name === 'email') return emailCheck();
+    else if (props.name === 'password') return passwordCheck();
+    else return true;
+  };
+
+  // 유효성에 따른 조건부 렌더링
+  const renderRegex = () => {
+    if (props.label === '이메일') {
+      return <AuthEmailCheck valid={emailCheck()} />;
+    }
+    if (props.label === '비밀번호') {
+      return <AuthPwCheck valid={passwordCheck()} />;
+    }
+  };
+
   return (
-    <div className="flex mb-4">
-      <Input
-        label={`${info}`}
-        name={enLabel}
-        onChange={handleChange}
-        placeholder={placeholder}
-        type={type}
-      />
-      <div className="flex sm:min-w-[5rem] min-w-[4rem] items-end justify-cente ml-4">
-        {renderButton()}
+    <div className="sm:mb-4 mb-2">
+      <div className="flex">
+        <Input
+          label={`${props.label}`}
+          name={props.name}
+          onChange={handleChange}
+          placeholder={props.placeholder}
+          type={props.type}
+          valid={renderValid()}
+        />
+        <div className="flex sm:min-w-[5rem] min-w-[4rem] items-end justify-cente ml-4">
+          {renderButton()}
+        </div>
       </div>
+      {renderRegex()}
     </div>
   );
 }
