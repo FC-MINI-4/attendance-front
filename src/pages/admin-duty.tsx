4122c7feaaea3886e57cfd4b6e3a57f45e673707
leechanghwi <lih120@naver.com>
2023-08-01 15:26:23 +0900

    FEAT:관리자페이지 데이트픽커 및 필터

--- src/pages/admin-duty.tsx
+++ src/pages/admin-duty.tsx
@@ -1,10 +1,13 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import Main from '@/components/admin/AdminMain';
+import { RecoilRoot } from 'recoil';
 
 export default function adminDuty() {
   return (
-    <div className="w-full h-screen bg-mainGray flex items-center mx-auto">
-      <Main page="admin-duty" />
-    </div>
+    <RecoilRoot>
+      <div className="w-full h-screen bg-mainGray flex items-center mx-auto">
+        <Main page="admin-duty" />
+      </div>
+    </RecoilRoot>
   );
 }
