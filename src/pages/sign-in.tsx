139279c8a329a16b39c06b1e74c340f1e2abfb66
Kled <1017yu@daum.net>
2023-08-01 21:14:29 +0900

    STYLE: 로고 임시 적용

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,10 +1,11 @@
 import AuthSignInBox from '@/components/auth/sign-in/AuthSignInBox';
+import Image from 'next/image';
+
 export default function signin() {
   return (
     <div className="mx-auto sm:w-[500px] sm:my-[90px]">
-      {' '}
       <div className="flex justify-center text-2xl sm:text-3xl bg-white sm:my-8 my-4">
-        회사명
+        <Image src="/logo.png" alt="logo" width={250} height={20} />
       </div>
       <AuthSignInBox />
     </div>
