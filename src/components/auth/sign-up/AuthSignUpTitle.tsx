862184fc55896a9b1300da258565a6b4a7308fe4
Kled <1017yu@daum.net>
2023-08-01 21:14:21 +0900

    STYLE: 로고 임시 적용

--- src/components/auth/sign-up/AuthSignUpTitle.tsx
+++ src/components/auth/sign-up/AuthSignUpTitle.tsx
@@ -1,7 +1,11 @@
+import Image from 'next/image';
+
 export default function AuthSignUpTitle() {
   return (
     <>
-      <div className="flex justify-center text-2xl sm:text-3xl">회원가입</div>
+      <div className="flex justify-center text-2xl sm:text-3xl">
+        <Image src="/logo.png" alt="logo" width={250} height={20} />
+      </div>
       <div className="flex mt-[1rem] justify-end mr-1">
         <div className="text-secondary">* &nbsp;</div>필수입력사항
       </div>
