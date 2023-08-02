65b5872f54f4773ed87ac5b8f04c28accf4f417c
Kled <1017yu@daum.net>
2023-08-02 13:39:55 +0900

    STYLE: 스타일 세부 수정

--- src/components/member/MemberHeader.tsx
+++ src/components/member/MemberHeader.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import Image from 'next/image';
 
 export default function MemberHeader() {
   return (
-    <div className="h-32 flex justify-between px-24 border-b border-black">
+    <div className="h-24 flex justify-between px-24 border-b shadow-sm">
       <div className="top-0 bottom-0 my-auto text-3xl font-bold">
-        <span className="text-primary">W</span>ork <span className="text-primary">M</span>anager
+        <Image src={'/logo.png'} width={150} height={150} alt="logo" />
       </div>
     </div>
-  )
+  );
 }
--- src/components/member/MemberInfo.tsx
+++ src/components/member/MemberInfo.tsx
@@ -1,29 +1,38 @@
-import Link from "next/link";
-import {HiOutlineUserCircle} from 'react-icons/hi'
-
+import Link from 'next/link';
+import { HiOutlineUserCircle } from 'react-icons/hi';
 
 const dummyData = {
-  "name": '문현수',
-  "department": "개발",
-  "position": "팀장", 
-  "employeeId": "YSL-001",
-  "hireDate": "2022-06-25",
-  "profileUrl": 'https://www.googleapis.com/1',
-}
+  name: '문현수',
+  department: '개발',
+  position: '팀장',
+  employeeId: 'YSL-001',
+  hireDate: '2022-06-25',
+  profileUrl: 'https://www.googleapis.com/1'
+};
 
 const handleClick = () => {
   // 1. 로그아웃 API를 요청합니다.
-  return
-}
+  return;
+};
 
 // 받아온 프로필URL을 Photo로 변환합니다.
 
 export default function MemberInfo() {
   return (
     <div className="flex flex-col text-lg">
-      <div>{dummyData.profileUrl.length > 0 ? dummyData.profileUrl : <HiOutlineUserCircle />}</div>
-      <div className="text-2xl mt-10">{dummyData.name}님, 반갑습니다.</div>
-      <button onClick={handleClick}><Link href={'/sign-in'}>로그아웃</Link></button>
+      <div>
+        {dummyData.profileUrl.length > 0 ? (
+          dummyData.profileUrl
+        ) : (
+          <HiOutlineUserCircle />
+        )}
+      </div>
+      <div className="text-2xl my-10 text-center">
+        {dummyData.name}님, 반갑습니다.
+      </div>
+      <button onClick={handleClick}>
+        <Link href={'/sign-in'}>로그아웃</Link>
+      </button>
     </div>
-  )
+  );
 }
