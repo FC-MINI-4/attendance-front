70ac702b955db0910844ccf81d171697f39aea9a
Kled <1017yu@daum.net>
2023-08-02 13:39:55 +0900

    STYLE: 스타일 세부 수정

--- src/pages/member.tsx
+++ src/pages/member.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import MemberInfo from '@/components/member/MemberInfo';
 import MemberBoard from '@/components/member/MemberBoard';
 import MemberHeader from '@/components/member/MemberHeader';
@@ -9,25 +9,21 @@ export default function Member() {
   return (
     <>
       <MemberHeader />
-      <div className="flex mx-24 my-24">
-        <div className="pb-10 mr-20 ">
-            <MemberBoard/>
-            <div className='mt-16'>
+      <div className="mx-24 mt-8 font-bold sm:text-4xl border-b-black border-b-4 sm:pb-1">
+        마이페이지
+      </div>
+      <div className="flex mx-24 my-12 justify-between">
+        <div className="flex">
+          <div className="flex flex-col sm:mr-32">
+            <MemberBoard />
             <MemberSideBar />
-            </div>
-        </div>
-        <div>
-          
-        </div>
-        <div className="mr-80">
+          </div>
           <MemberDetail />
-        </div>  
+        </div>
         <div>
           <MemberInfo />
-          </div>
+        </div>
       </div>
-      
-      
-     </>
+    </>
   );
 }
