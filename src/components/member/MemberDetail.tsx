7419ce62ea1ffb52ad1b4e635c7fdd7aae5f2385
Kled <1017yu@daum.net>
2023-08-02 13:39:48 +0900

    FIX: 하드코딩 반복문으로 수정

--- src/components/member/MemberDetail.tsx
+++ src/components/member/MemberDetail.tsx
@@ -1,23 +1,23 @@
 // new Date()로 받아오는 현재 날짜 데이터를
 // toLocaleString() // 한국 시각데이터로 바꿉니다.
-// 2023.8.1 오후 2:17:38 
+// 2023.8.1 오후 2:17:38
 
 // 2023 8 1 - 2022 06 15 = 1년 1개월
 
 // 현재 날짜와 시간 생성
-const currentDate = new Date()
+const currentDate = new Date();
 
 const dummyData = {
-  "name": '문현수',
-  "department": "개발",
-  "position": "팀장", 
-  "employeeId": "YSL-001",
-  "hireDate": "2022-06-25" 
-}
+  name: '문현수',
+  department: '개발',
+  position: '팀장',
+  employeeId: 'YSL-001',
+  hireDate: '2022-06-25'
+};
 
 /////////////////////////
 
-// 근속 기간을 계산합니다. 
+// 근속 기간을 계산합니다.
 function calculateDuration(startDate: Date, endDate: Date) {
   let yearsDiff = endDate.getFullYear() - startDate.getFullYear();
   let monthsDiff = endDate.getMonth() - startDate.getMonth();
@@ -28,7 +28,7 @@ function calculateDuration(startDate: Date, endDate: Date) {
     monthsDiff = 12 - startDate.getMonth() + endDate.getMonth();
   }
 
-  return yearsDiff + "년 " + monthsDiff + "개월";
+  return yearsDiff + '년 ' + monthsDiff + '개월';
 }
 
 const duration = calculateDuration(new Date(dummyData.hireDate), currentDate);
@@ -39,27 +39,47 @@ const duration = calculateDuration(new Date(dummyData.hireDate), currentDate);
 
 // 검색 기준일 (2023.8.1 (화)) 형식 반환 함수
 const formatSearchDate = (date: Date) => {
-  const days = ["일", "월", "화", "수", "목", "금", "토"];
-  const daysText = days[date.getDay()] // 현재 요일 인덱스를 반환합니다.
+  const days = ['일', '월', '화', '수', '목', '금', '토'];
+  const daysText = days[date.getDay()]; // 현재 요일 인덱스를 반환합니다.
 
-  return date.toLocaleString('ko-KR', { year: 'numeric', month: 'numeric', day: 'numeric' }) + " (" + daysText + ")";
-}
+  return (
+    date.toLocaleString('ko-KR', {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric'
+    }) +
+    ' (' +
+    daysText +
+    ')'
+  );
+};
 
-const searchDate = formatSearchDate(currentDate)
+const searchDate = formatSearchDate(currentDate);
 
 /////////////////////////
 
+const memberDetail = {
+  부서명: dummyData.department,
+  직위: dummyData.position,
+  사원번호: dummyData.employeeId,
+  입사일: dummyData.hireDate,
+  '근무 기간': duration,
+  '검색 기준일': searchDate
+};
 
 export default function MemberDetail() {
   return (
     <div>
-      <div className="text-lg">부서명: {dummyData.department}</div>
-      <div className="text-lg mt-8">직위: {dummyData.position}</div>
-      <div className="text-lg mt-8">사원번호: {dummyData.employeeId}</div>
-      <div className="text-lg mt-8">입사일: {dummyData.hireDate}</div>
-      <div className="text-lg mt-8">근무 기간: {duration}</div>
-      <div className="text-lg mt-8">검색 기준일: {searchDate}</div>
+      {Object.entries(memberDetail).map(([key, value]) => (
+        <>
+          <div key={key} className="sm:text-sm sm:pb-2 text-subTextAndBorder">
+            {key}
+          </div>
+          <div className="sm:mb-6 sm:min-w-[20rem] sm:pb-2 text-xl border-b border-mainGray">
+            {value}
+          </div>
+        </>
+      ))}
     </div>
-  )
+  );
 }
-
--- src/components/member/MemberSideBar.tsx
+++ src/components/member/MemberSideBar.tsx
@@ -1,28 +1,29 @@
-export default function MemberSideBar(){
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-  const Side = () => {
-    const SideBar = []
-    const sideList = ['마이페이지', '개인정보수정', '연차/반차 신청 리스트']
-    
-    for(let i=0; i < 3; i++){
-      SideBar.push(
+export default function MemberSideBar() {
+  const router = useRouter(); // useRouter()를 통해 현재 pathname을 변수 router에 저장
+
+  const sideList = {
+    마이페이지: '/member',
+    개인정보수정: '/member/edit',
+    '연차/반차 신청 리스트': '/member/list'
+  };
+  return (
+    <div className="sm:mt-8">
+      {Object.entries(sideList).map(([key, link]) => (
         <div
-          key={i}
-          className="before:content-[''] before:block before:w-1.5 before:h-6 before:bg-mainBlack before:absolute before:top-3.5 before:left-[10px] before:hover:bg-primary
-          py-3.5 relative text-mainBlack pl-8 font-semibold cursor-pointer
-          hover:text-primary"
-        >
-          {sideList[i]}
+          key={key}
+          className={`before:content-[''] before:block before:w-1.5 before:h-6 before:bg-mainBlack before:absolute before:top-3.5 before:left-[10px] before:hover:bg-primary
+        py-3.5 relative text-mainBlack pl-8 font-semibold cursor-pointer
+        hover:text-primary ${
+          link === router.pathname
+            ? 'text-primary before:bg-primary'
+            : 'text-mainBlack hover:text-primary'
+        }`}>
+          <Link href={link}>{key}</Link>
         </div>
-      )
-    }
-    return(
-    <>
-      <div>{SideBar}</div>
-    </>
-    )
-  }
-  return(
-    <div><Side/></div>
-  )
+      ))}
+    </div>
+  );
 }
