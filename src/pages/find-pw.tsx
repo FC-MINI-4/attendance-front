ae309c08b4d1d67552ac517348a3b050a4a69000
Kled <1017yu@daum.net>
2023-08-03 15:59:53 +0900

    FEAT: Auth Layout 추가

--- src/pages/find-pw.tsx
+++ src/pages/find-pw.tsx
@@ -1,13 +1,10 @@
+import AuthLayout from '@/components/auth/AuthLayout';
 import AuthFindPwBox from '@/components/auth/AuthFindPwBox';
-import Image from 'next/image';
 
 export default function changePw() {
   return (
-    <div className="mx-auto sm:w-[580px] sm:my-[90px]">
-      <div className="flex justify-center text-2xl sm:text-4xl bg-white sm:my-8">
-        <Image src="/logo.png" alt="logo" width={250} height={20} />
-      </div>
+    <AuthLayout>
       <AuthFindPwBox />
-    </div>
+    </AuthLayout>
   );
 }
