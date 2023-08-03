9fc34d25875dfedea4f97251fa315d665a27620c
Kled <1017yu@daum.net>
2023-08-03 16:14:42 +0900

    FEAT: 비밀번호 찾기 이메일 유효성 검사

--- src/components/auth/AuthFindPwInput.tsx
+++ src/components/auth/AuthFindPwInput.tsx
@@ -1,16 +1,32 @@
 import Input from '@/components/common/Input';
+import Button from '@/components/common/Button';
+import { useState } from 'react';
+import { rEmail } from '@/constants/constants';
 
 export default function AuthFindPwInput() {
-  const onClick = () => {};
+  const [email, setEmail] = useState('');
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event?.target.value);
+  };
+
+  // 이메일 형식 유효성 체크
+  const emailCheck = () => {
+    return rEmail.test(email);
+  };
+
   return (
-    <div className="mb-16">
-      <Input
-        label={'이메일'}
-        name={'email'}
-        onChange={onClick}
-        placeholder={'예: jindojoon@soonyang.com'}
-        valid={true}
-      />
-    </div>
+    <>
+      <div className="mb-16">
+        <Input
+          label={'이메일'}
+          name={'email'}
+          onChange={onChange}
+          placeholder={'예: jindojoon@soonyang.com'}
+          valid={true}
+        />
+      </div>
+      <Button contents={'이메일 전송'} disabled={!emailCheck()} />
+    </>
   );
 }
