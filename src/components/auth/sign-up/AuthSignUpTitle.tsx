6d5cb2898de09c109c8911e6b0fa2be0953bdbb2
Kled <1017yu@daum.net>
2023-08-03 12:45:54 +0900

    STYLE: 세부 수정

--- src/components/auth/sign-up/AuthSignUpTitle.tsx
+++ src/components/auth/sign-up/AuthSignUpTitle.tsx
@@ -7,10 +7,10 @@ export default function AuthSignUpTitle() {
         <Image src="/logo.png" alt="logo" width={250} height={20} />
       </div>
       <div className="flex mt-[1rem] justify-end mr-1">
-        <div className="text-secondary">* &nbsp;</div>필수입력사항
+        * &nbsp;필수입력사항
       </div>
-      <div className=" mb-8">
-        <hr className="font-bold border-b border-gray-700 " />
+      <div className="mb-8">
+        <hr className="font-bold border-b border-gray-700" />
       </div>
     </>
   );
