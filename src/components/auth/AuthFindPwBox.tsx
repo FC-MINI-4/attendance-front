d9e0a9ca682e616ca989a872b9c1889f894d4bca
Kled <1017yu@daum.net>
2023-08-03 16:14:42 +0900

    FEAT: 비밀번호 찾기 이메일 유효성 검사

--- src/components/auth/AuthFindPwBox.tsx
+++ src/components/auth/AuthFindPwBox.tsx
@@ -1,5 +1,4 @@
 import PwBox from '@/components/common/PwBox';
-import Button from '@/components/common/Button';
 import AuthFindPwInput from '@/components/auth/AuthFindPwInput';
 
 export default function AuthFindPwBox() {
@@ -9,13 +8,11 @@ export default function AuthFindPwBox() {
         순양 계정의
         <br /> 비밀번호를 재설정 합니다.
       </div>
-      <div className="text-subTextAndBorder mb-8 text-sm">
-        비밀번호를 재설정할 계정의 이메일을 입력해주세요.
-        <br />
-        가입하신 이메일로 임시 비밀번호를 발송해드립니다.
+      <div className="text-subTextAndBorder mb-8 text-base">
+        비밀번호를 재설정할 <br />
+        계정의 이메일을 입력해주세요.
       </div>
       <AuthFindPwInput />
-      <Button contents={'이메일 전송'} />
     </PwBox>
   );
 }
