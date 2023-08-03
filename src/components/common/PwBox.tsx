38fcfcb8de508808e3f70b6575c46202e9951132
Kled <1017yu@daum.net>
2023-08-03 12:45:12 +0900

    STYLE: 스타일 세부 수정

--- src/components/common/PwBox.tsx
+++ src/components/common/PwBox.tsx
@@ -2,9 +2,7 @@ import { ILayout } from '@/types/ICommon';
 
 export default function PwBox({ children, ...props }: ILayout) {
   return (
-    <div
-      {...props}
-      className="border border-subTextAndBorder sm:p-16 p-8 rounded-md sm:mt-12">
+    <div {...props} className="sm:mt-4">
       {children}
     </div>
   );
