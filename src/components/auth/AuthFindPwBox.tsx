faccf70ff406ece8b91d3328446d1f91e89aa3f3
Kled <1017yu@daum.net>
2023-08-03 15:59:42 +0900

    STYLE: 반응형 수정

--- src/components/auth/AuthFindPwBox.tsx
+++ src/components/auth/AuthFindPwBox.tsx
@@ -5,12 +5,12 @@ import AuthFindPwInput from '@/components/auth/AuthFindPwInput';
 export default function AuthFindPwBox() {
   return (
     <PwBox>
-      <div className="sm:mb-8 sm:text-3xl">
+      <div className="mb-8 text-3xl">
         순양 계정의
         <br /> 비밀번호를 재설정 합니다.
       </div>
-      <div className="text-subTextAndBorder  sm:mb-8">
-        비밀번호를 재설정할 계정의 <br /> 이메일을 입력해주세요. <br />
+      <div className="text-subTextAndBorder mb-8 text-sm">
+        비밀번호를 재설정할 계정의 이메일을 입력해주세요.
         <br />
         가입하신 이메일로 임시 비밀번호를 발송해드립니다.
       </div>
--- src/components/auth/AuthFindPwInput.tsx
+++ src/components/auth/AuthFindPwInput.tsx
@@ -3,12 +3,13 @@ import Input from '@/components/common/Input';
 export default function AuthFindPwInput() {
   const onClick = () => {};
   return (
-    <div className="sm:mb-16">
+    <div className="mb-16">
       <Input
         label={'이메일'}
         name={'email'}
         onChange={onClick}
         placeholder={'예: jindojoon@soonyang.com'}
+        valid={true}
       />
     </div>
   );
