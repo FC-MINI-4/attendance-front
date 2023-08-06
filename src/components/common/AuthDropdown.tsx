f0ba5eb7299a14c245cb08065418f16dd8f0af89
Kled <1017yu@daum.net>
2023-08-06 14:12:24 +0900

    FEAT: 계열사 업데이트

--- src/components/common/AuthDropdown.tsx
+++ src/components/common/AuthDropdown.tsx
@@ -1,13 +1,21 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { IDropdown } from '@/types/ICommon';
 import { signUpState } from '@/recoil/signUp';
-
 import { Listbox, Transition } from '@headlessui/react';
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
 
 export default function AuthDropdown({ ...props }: IDropdown) {
-  const [selected, setSelected] = useState(props.options[0]);
+  const [selected, setSelected] = useState(Object.keys(props.options)[0]);
+  const setSignUpInfo = useSetRecoilState(signUpState);
+
+  // 계열사 dropdown 업데이트
+  useEffect(() => {
+    setSignUpInfo(prevInformation => ({
+      ...prevInformation,
+      ['department']: props.options[selected]
+    }));
+  }, [props.options, selected, setSignUpInfo]);
 
   return (
     <div className="pt-2 text-left border-b-2 h-10 border-gray-200 mb-4 max-w-[calc(100%-5rem)] sm:max-w-[calc(100%-6rem)]">
@@ -30,26 +38,21 @@ export default function AuthDropdown({ ...props }: IDropdown) {
             <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
               {Object.entries(props.options).map(([key, value]) => (
                 <Listbox.Option
-                  key={key}
+                  key={value}
                   className={({ active }) =>
                     `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                      active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                      active ? 'bg-primary text-white' : 'text-gray-900'
                     }`
                   }
-                  value={value}>
+                  value={key}>
                   {({ selected }) => (
                     <>
                       <span
                         className={`block truncate ${
                           selected ? 'font-medium' : 'font-normal'
                         }`}>
-                        {value}
+                        {key}
                       </span>
-                      {selected ? (
-                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                        </span>
-                      ) : null}
                     </>
                   )}
                 </Listbox.Option>
