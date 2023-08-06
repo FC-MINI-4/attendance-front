b26fe0f1e53ac39769fc5ab3ccbce9918ebf7f3f
Kled <1017yu@daum.net>
2023-08-06 14:14:47 +0900

    FEAT: 회원가입 폼 계열사 선택 추가

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { RecoilRoot } from 'recoil';
 import Button from '@/components/common/Button';
 import { TEMP_DEPARTMENT } from '@/constants/option';
@@ -9,20 +8,15 @@ import AuthSignUpInput from '@/components/auth/sign-up/AuthSignUpInput';
 import AuthSignUpTitle from '@/components/auth/sign-up/AuthSignUpTitle';
 
 export default function SignUp() {
-  const [selectedDuty, setSelectedDuty] = useState<string>('계열사');
-
-  const handleDutyChange = (value: string) => {
-    setSelectedDuty(value);
-  };
-
   return (
     <RecoilRoot>
       <div className="mx-auto sm:w-[500px] sm:my-[90px]">
         <AuthSignUpTitle />
+        <div className="text-xs sm:text-base font-semibold pl-1 text-mainBlack">
+          계열사*
+        </div>
         <AuthDropdown
           options={TEMP_DEPARTMENT}
-          value={selectedDuty}
-          onChange={handleDutyChange}
           label={'계열사'}
           admin={false}
         />
@@ -36,8 +30,10 @@ export default function SignUp() {
             type={value.type}
           />
         ))}
-        <div className="text-xs sm:text-base font-semibold ">입사일</div>
-        <div className="border-b-2 border-gray-200 mb-4 sm:w-full sm:max-w-[calc(100%-6rem)]">
+        <div className="text-xs sm:text-base font-semibold pl-1 text-mainBlack">
+          입사일
+        </div>
+        <div className="border-b-2 border-gray-200 mb-4 sm:w-full sm:max-w-[calc(100%-6rem)] pl-1">
           <SinglePicker name={'hireDate'} />
         </div>
         <Button contents={'회원가입'} />
