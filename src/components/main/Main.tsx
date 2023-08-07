422a2b5f8cb0c0f5206b57b428b30a03caba24f9
96uoow <mhs0704123@naver.com>
2023-08-07 19:32:18 +0900

    feat:연차/당직신청api설정

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -13,11 +13,13 @@ export default function Main(){
       <div className='absolute top-0 left-0 w-full shadow-md'>
         <MainHeader/>
       </div>
-      <div className='absolute top-[8.5rem] left-0 w-1/6 pt-10 pl-3 pr-3'>
-        <SideMenu/>
-      </div>
-      <div className='absolute top-[8.5rem] right-0 w-5/6'>
-        <Calendar/>
+      <div className='w-5/6 absolute top-[8.5rem] left-0 right-0 mx-auto'>
+        <div className='absolute top-0 left-0 w-1/6 pt-10 pl-3 pr-3'>
+          <SideMenu/>
+        </div>
+        <div className='absolute top-0 right-0 w-5/6'>
+          <Calendar/>
+        </div>
       </div>
     </div>
   </>
