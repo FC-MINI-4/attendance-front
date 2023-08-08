5b159f01f452717fa63bc29ce70c7e4c32902501
Kled <1017yu@daum.net>
2023-08-09 03:40:44 +0900

    FEAT: 쿠키 프로바이더 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/pages/index.css';
 import type { AppProps } from 'next/app';
+import { CookiesProvider } from 'react-cookie';
 import Layout from '@/components/common/Layout';
-
 
 export default function App({ Component, pageProps, router }: AppProps) {
   //여기에 페이지주소 적으면 해당 페이지는 전역레이아웃사용하지않습니다.
@@ -21,8 +21,10 @@ export default function App({ Component, pageProps, router }: AppProps) {
   }
 
   return (
+    <CookiesProvider>
       <Layout>
         <Component {...pageProps} />
       </Layout>
+    </CookiesProvider>
   );
 }
