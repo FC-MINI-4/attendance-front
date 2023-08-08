0b63562fdc4ad9a8a614396ba35ff6b18f9ba047
Kled <1017yu@daum.net>
2023-08-08 14:28:14 +0900

    FIX: 관리자 서버 미 가동에 따른 axios 세팅 수정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,17 +2,11 @@ import axios, { AxiosError } from 'axios';
 
 const apiIP = process.env.NEXT_PUBLIC_API_IP;
 const clientPort = process.env.NEXT_PUBLIC_API_CLIENT_PORT;
-const adminPort = process.env.NEXT_PUBLIC_API_ADMIN_PORT;
 
 const clientUrl = `http://${apiIP}:${clientPort}`;
-const adminUrl = `http://${apiIP}:${adminPort}`;
 
 export const clientInstance = axios.create({
   baseURL: `${clientUrl}`
-});
-
-export const adminInstance = axios.create({
-  baseURL: `${adminUrl}`
 });
 
 clientInstance.interceptors.request.use(
@@ -25,24 +19,6 @@ clientInstance.interceptors.request.use(
 );
 
 clientInstance.interceptors.response.use(
-  function (response) {
-    return response;
-  },
-  function (error: AxiosError) {
-    return Promise.reject(error.response?.data);
-  }
-);
-
-adminInstance.interceptors.request.use(
-  function (config) {
-    return config;
-  },
-  function (error: AxiosError) {
-    return Promise.reject(error);
-  }
-);
-
-adminInstance.interceptors.response.use(
   function (response) {
     return response;
   },
