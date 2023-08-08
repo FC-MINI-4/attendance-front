da325b70b207f89fe96bd850cf7b325efc47a1f1
96uoow <mhs0704123@naver.com>
2023-08-09 08:00:55 +0900

    feat:달력기능추가

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -9,12 +9,12 @@ export default function Main(){
 
   return(
   <>
-    <div className='w-screen h-screen position'>
+    <div className='w-screen h-screen relative'>
       <div className='absolute top-0 left-0 w-full shadow-md'>
         <MainHeader/>
       </div>
-      <div className='w-5/6 absolute top-[8.5rem] left-0 right-0 mx-auto'>
-        <div className='absolute top-0 left-0 w-1/6 pt-10 pl-3 pr-3'>
+      <div className='w-11/12 absolute top-[7rem] left-0 right-0 mx-auto'>
+        <div className='absolute top-0 left-0 w-1/6 pt-10'>
           <SideMenu/>
         </div>
         <div className='absolute top-0 right-0 w-5/6'>
