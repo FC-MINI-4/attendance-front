40e6b86c504ddb7e06e910894d8b6e8f50964355
Kled <1017yu@daum.net>
2023-08-10 06:53:02 +0900

    FIX: 마이페이지 수정

--- src/components/member/MyPageList.tsx
+++ src/components/member/MyPageList.tsx
@@ -1,79 +1,84 @@
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-// import { MypageLeaveResIProps, MyPageDutyResIProps } from '@/types/IMyPages';
+import React, { useEffect, useState } from 'react';
+import { IMemberDayOffProps, IMemberDutyProps } from '@/types/IMyPages';
+import memberList from '@/api/member/memberList';
 
-// export default function MyPageList() {
-//   const [dutyEmployees, setDutyEmployees] = useState<MyPageDutyResIProps[]>([]);
-//   const [restEmployees, setRestEmployees] = useState<MypageLeaveResIProps[]>(
-//     []
-//   );
+export default function MyPageList() {
+  const [dayOffEmployees, setDayOffEmployees] = useState<IMemberDayOffProps[]>(
+    []
+  );
+  const [dutyEmployees, setDutyEmployees] = useState<IMemberDutyProps[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-//   useEffect(() => {
-//     const fetchEmployees = async () => {
-//       try {
-//         const [dutyResponse, restResponse] = await Promise.all([
-//           axios.get('/api/WorkList'),
-//           axios.get('/api/RestList')
-//         ]);
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
 
-//         setDutyEmployees(dutyResponse.data.data?.duties || []);
-//         setRestEmployees(restResponse.data.data?.dayOffs || []);
-//       } catch (error) {
-//         console.error('API 호출 오류:', error);
-//       }
-//     };
+      const response = await memberList();
+      const resList = response.data || {};
 
-//     fetchEmployees();
-//   }, []);
+      if (resList.duties) {
+        setDutyEmployees(resList.duties);
+      }
+      if (resList.dayOffs) {
+        setDayOffEmployees(resList.dayOffs);
+      }
+      {
+        setTimeout(() => setIsLoading(false), 500);
+      }
+    };
 
-//   return (
-//     <div className="w-[64rem] h-[36.67rem]">
-//       <div className="relative bg-gray-200 w-1/6 rounded-sm-lg font-bold sm:text-2xl sm:pb-8 h-9">
-//         <span className="bg-primary absolute top-0 left-0 w-4 h-9 z-0"></span>
-//         <span className="relative z-10 pl-4">연차 리스트</span>
-//       </div>
+    fetchData();
+  }, []);
 
-//       <div className="flex justify-between border-y-2 border-solid mt-6 text-1.5xl font-semibold py-3 text-center">
-//         <div className="w-[13.33rem] ">요청</div>
-//         <div className="w-[16.67rem]">사용날짜</div>
-//         <div className="w-[16.67rem]">사유</div>
-//         <div className="w-[13.33rem]">처리결과</div>
-//       </div>
-//       <div className="w-[64rem] h-[11.67rem] overflow-y-auto ">
-//         {restEmployees.map(dayoffs => (
-//           <div
-//             key={dayoffs.employeeId}
-//             className="h-[3.33rem] flex justify-between border-b-2 border-solid text-1.5xl py-3 text-secondaryGray text-center ">
-//             <div className="w-[13.33rem]">{dayoffs.type}</div>
-//             <div className="w-[16.67rem]">{`${dayoffs.startDate} ~ ${dayoffs.endDate}`}</div>
-//             <div className="w-[16.67rem]">{dayoffs.reason}</div>
-//             <div className="w-[13.33rem]">{dayoffs.status}</div>
-//           </div>
-//         ))}
-//       </div>
+  return (
+    <div className="w-[64rem] h-[36.67rem]">
+      <div className="relative bg-gray-200 w-1/6 rounded-sm-lg font-bold sm:text-2xl sm:pb-8 h-9">
+        <span className="bg-primary absolute top-0 left-0 w-4 h-9 z-0"></span>
+        <span className="relative z-10 pl-4 ml-2">연차 리스트</span>
+      </div>
+      <div className="flex justify-between border-y-2 border-solid mt-6 text-1.5xl font-semibold py-3 text-center">
+        <div className="w-[13.33rem] ">요청</div>
+        <div className="w-[16.67rem]">사용날짜</div>
+        <div className="w-[16.67rem]">사유</div>
+        <div className="w-[13.33rem]">처리결과</div>
+      </div>
+      <div className="w-[64rem] h-[11.67rem] overflow-y-auto ">
+        {dayOffEmployees.map(dayoffs => (
+          <div
+            key={dayoffs.dayOffId}
+            className="h-[3.33rem] flex justify-between border-b-2 border-solid text-1.5xl py-3 text-secondaryGray text-center ">
+            <div className="w-[13.33rem]">
+              {dayoffs.type}({dayoffs.amount}일)
+            </div>
+            <div className="w-[16.67rem]">{`${dayoffs.startDate} ~ ${dayoffs.endDate}`}</div>
+            <div className="w-[16.67rem]">{dayoffs.reason}</div>
+            <div className="w-[13.33rem]">{dayoffs.status}</div>
+          </div>
+        ))}
+      </div>
+      <div className="relative bg-gray-200 w-1/6 rounded-sm font-bold sm:text-2xl sm:pb-8 h-9 mt-10">
+        <span className="bg-primary absolute top-0 left-0 w-4 h-9 z-0"></span>
+        <span className="relative z-10 pl-4 ml-2">당직 리스트</span>
+      </div>
+      <div>
+        <div className="flex justify-between border-y-2 border-solid mt-6 text-1.5xl font-semibold py-3 text-center">
+          <div className="w-[13.33rem]">요청</div>
+          <div className="w-[33.33rem]">사용날짜</div>
+          <div className="w-[13.33rem]"> 처리결과</div>
+        </div>
 
-//       <div className="relative bg-gray-200 w-1/6 rounded-sm font-bold sm:text-2xl sm:pb-8 h-9 mt-10">
-//         <span className="bg-primary absolute top-0 left-0 w-4 h-9 z-0"></span>
-//         <span className="relative z-10 pl-4">당직 리스트</span>
-//       </div>
-//       <div>
-//         <div className="flex justify-between border-y-2 border-solid mt-6 text-1.5xl font-semibold py-3 text-center">
-//           <div className="w-[13.33rem]">요청</div>
-//           <div className="w-[33.33rem]">사용날짜</div>
-//           <div className="w-[13.33rem]"> 처리결과</div>
-//         </div>
-//         <div className="w-[64rem] h-[11.67rem] overflow-y-auto">
-//           {dutyEmployees.map(duties => (
-//             <div
-//               key={duties.employeeId}
-//               className="flex justify-between border-b-2 border-solid text-1.5xl py-3 text-secondaryGray overflow-y-auto text-center">
-//               <div className="w-[13.33rem]">{duties.type}</div>
-//               <div className="w-[33.33rem]">{duties.date}</div>
-//               <div className="w-[13.33rem]">{duties.status}</div>
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+        <div className="w-[64rem] h-[11.67rem] overflow-y-auto">
+          {dutyEmployees.map(duties => (
+            <div
+              key={duties.dutyId}
+              className="flex justify-between border-b-2 border-solid text-1.5xl py-3 text-secondaryGray overflow-y-auto text-center">
+              <div className="w-[13.33rem]">{duties.type}</div>
+              <div className="w-[33.33rem]">{duties.date}</div>
+              <div className="w-[13.33rem]">{duties.status}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
