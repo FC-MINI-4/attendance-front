be051bf94fbe2fa58203d36146ff063448df40a6
Kled <1017yu@daum.net>
2023-08-10 08:08:17 +0900

    FEAT: cookie에서 employeeId get

--- src/api/auth/changePw.ts
+++ src/api/auth/changePw.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import { clientInstance } from '@/api/axios';
-import { IAuthCheckPw } from '@/types/IAuth';
+import { IAuthChangePw } from '@/types/IAuth';
 
 // * [POST] 비밀번호 변경
-export async function requestChangePw(changePwData: IAuthCheckPw) {
+export async function requestChangePw(changePwData: IAuthChangePw) {
   try {
     const response = await axios.post(
       `${clientInstance.defaults.baseURL}/api/auth/users/password/change`,
