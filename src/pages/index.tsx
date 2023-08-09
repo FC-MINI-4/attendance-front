ade281f12e036aea09a870fad323f40a792eea61
Kled <1017yu@daum.net>
2023-08-09 12:20:43 +0900

    FEAT: 최초 렌더링 리다이렉션

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
 import Button from '@/components/common/Button';
+import { GetServerSideProps } from 'next';
 import Link from 'next/link';
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  return {
+    redirect: {
+      destination: '/sign-in', // 리다이렉션할 페이지 경로
+      permanent: true // 301 리다이렉션 설정
+    }
+  };
+};
 
 export default function Home() {
   return (
