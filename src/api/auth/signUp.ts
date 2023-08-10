0d6ba5eeb17bc5cd1034e06b6b3e98c49cdb7989
Kled <1017yu@daum.net>
2023-08-10 11:20:03 +0900

    FIX: 절대 경로 수정

--- src/api/auth/signUp.ts
+++ src/api/auth/signUp.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { clientInstance } from '../axios';
+import { clientInstance } from '@/api/axios';
 import { ISignInRequestBody } from '@/types/ISignIn';
 import { ISignUpRequestBody } from '@/types/ISignUp';
 
