0c4b31c0567f0c768b2b240fa681ea470d2555bf
Kled <1017yu@daum.net>
2023-08-12 23:13:58 +0900

    FEAT: 이메일 전송 버튼 후 전송완료 페이지

--- src/components/auth/find-pw/AuthSentEmailBox.tsx
+++ src/components/auth/find-pw/AuthSentEmailBox.tsx
@@ -1,14 +1,43 @@
-import PwBox from '@/components/common/PwBox';
+import AuthBox from '@/components/common/AuthBox';
+import SearchSiteButton from '@/components/common/SearchSiteButton';
 
 export default function AuthSentEmailBox() {
+  const handleNaverClick = () => {
+    window.location.href = 'https://www.naver.com';
+  };
+  const handleGoogleClick = () => {
+    window.location.href = 'https://www.google.com';
+  };
+
+  const handleKakaoClick = () => {
+    window.location.href = 'https://mail.kakao.com/';
+  };
+
   return (
-    <PwBox>
-      <div className="mb-8 text-3xl flex justify-center">
+    <AuthBox>
+      <div className="mb-8 text-3xl flex justify-center ">
         입력하신 이메일로 비밀번호가 전송되었습니다.
       </div>
       <div className="text-subTextAndBorder mb-8 text-xl flex justify-center">
         입력하신 이메일을 확인하여 계정의 비밀번호를 재설정하세요.
       </div>
-    </PwBox>
+      <div className="flex gap-4 sm:mt-16">
+        <SearchSiteButton
+          contents={'네이버'}
+          onClick={handleNaverClick}
+          site={'naver'}
+        />
+        <SearchSiteButton
+          contents={'구글'}
+          onClick={handleGoogleClick}
+          site={'google'}
+        />
+        <SearchSiteButton
+          contents={'카카오'}
+          onClick={handleKakaoClick}
+          site={'kakao'}
+        />
+      </div>
+    </AuthBox>
   );
 }
